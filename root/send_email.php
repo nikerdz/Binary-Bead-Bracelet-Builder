8cefa485a24bd5129ec4b65fa2794d6e3d48ef06
Anika Khan <anika.khan323@gmail.com>
2025-10-25 01:38:12 -0400

    fix: fixed png of bead pattern not loading in emails

--- root/send_email.php
+++ root/send_email.php
@@ -1,5 +1,4 @@
 <?php
-// Get the POSTed JSON data
 $data = json_decode(file_get_contents('php://input'), true);
 
 if (!isset($data['email']) || !isset($data['imageData'])) {
@@ -10,37 +9,35 @@ if (!isset($data['email']) || !isset($data['imageData'])) {
 $to = $data['email'];
 $from = "wics@uwindsor.ca";
 $subject = "Your WiCS Binary Bead Layout";
-$message = "Hi! \n Attached is your custom binary bead layout from the Women in Computer Science Club. \n Hope you love  your bracelet and enjoy the Open House! \n\n - WiCS at University of Windsor";
+$message = "Hi! \n\nAttached is your custom binary bead layout from the Women in Computer Science Club. \nHope you love your new bracelet and enjoy the Open House! \n\n - WiCS at University of Windsor";
 
-// Decode the base64 image
+// Decode base64 image
 $imageData = $data['imageData'];
 $imageParts = explode(";base64,", $imageData);
 $imageBase64 = $imageParts[1];
 $imageDecoded = base64_decode($imageBase64);
 
-// Create a unique filename
-$filename = "my-binary-bead-layout.png";
+// Create a temporary file
+$tmpFile = tempnam(sys_get_temp_dir(), 'bead') . '.png';
+file_put_contents($tmpFile, $imageDecoded);
 
-// Boundary for multipart email
+// Create headers for attachment
 $separator = md5(time());
-
-// Headers
 $headers  = "From: WiCS <{$from}>\r\n";
 $headers .= "MIME-Version: 1.0\r\n";
 $headers .= "Content-Type: multipart/mixed; boundary=\"{$separator}\"\r\n";
 
-// Message body
+// Body with text + attachment
 $body = "--{$separator}\r\n";
 $body .= "Content-Type: text/plain; charset=\"UTF-8\"\r\n";
 $body .= "Content-Transfer-Encoding: 7bit\r\n\r\n";
 $body .= $message . "\r\n";
 
-// Attachment
 $body .= "--{$separator}\r\n";
-$body .= "Content-Type: image/png; name=\"{$filename}\"\r\n";
+$body .= "Content-Type: image/png; name=\"bead_layout.png\"\r\n";
 $body .= "Content-Transfer-Encoding: base64\r\n";
-$body .= "Content-Disposition: attachment; filename=\"{$filename}\"\r\n\r\n";
-$body .= chunk_split(base64_encode($imageDecoded)) . "\r\n";
+$body .= "Content-Disposition: attachment; filename=\"bead_layout.png\"\r\n\r\n";
+$body .= chunk_split(base64_encode(file_get_contents($tmpFile))) . "\r\n";
 $body .= "--{$separator}--";
 
 // Send email
@@ -49,4 +46,8 @@ if (mail($to, $subject, $body, $headers)) {
 } else {
     echo "Failed to send email.";
 }
+
+// Delete temporary file
+unlink($tmpFile);
 ?>
+
